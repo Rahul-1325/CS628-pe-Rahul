3d3d429c8edb8eabd5157a599aa7b0d7a9052c9f
Rahul-1325 <rahul.dondapati@gmail.com>
2025-02-27 02:03:18 +0530

    Update CitiesList.js

--- PE04-Cities/cities/src/CitiesList.js
+++ PE04-Cities/cities/src/CitiesList.js
@@ -1,18 +1,20 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
-const CitiesList = ({ cities }) => {
-  return (
-    <div>
-      <h2>Cities List</h2>
-      <ul>
-        {cities.map((city) => (
-          <li key={city.id}>
-            <Link to={`/cities/${city.id}`}>{city.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+function CitiesList({ cities }) {
+    return (
+        <div>
+            <h2>List of Cities</h2>
+            <ul>
+                {cities.map((city) => (
+                    <li key={city.id}>
+                        <Link to={`/cities/${city.id}`}>{city.name}</Link>
+                    </li>
+                ))}
+            </ul>
+            <Outlet /> {/* Nested route for city details */}
+        </div>
+    );
+}
 
 export default CitiesList;
